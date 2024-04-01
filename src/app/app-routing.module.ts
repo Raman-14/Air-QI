5a48afdd134b35e500d884e40b9d696217819438
Raman-14 <ramanofficial014@gmail.com>
2024-04-01 19:13:54 +0530

    f

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WorkComponent } from './pages/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full',
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+    title:'Home-page',
+  },
+  {
+    path:'work',
+    component:WorkComponent,
+    title:'Work-page',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
